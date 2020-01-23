8f6eff4b2d1da5b86d43a673cdd5ad812f5f7e5d
danielafriyie <55259794+grandpaSenior@users.noreply.github.com>
2020-01-23 22:11:42 +0000

    changed about picture

--- grandpa/views.py
+++ grandpa/views.py
@@ -3,11 +3,10 @@ from . import models
 
 
 def index(request):
-    user_profile = models.MyProfile.objects.all()
-    work_portfolio = models.MyWorkPortfolio.objects.all()
+    user_profile = models.MyProfile.objects.all().order_by('-id').first()
+    work_portfolio = models.MyWorkPortfolio.objects.all().order_by('-id')
     context = {
         'userProfile': user_profile,
         'workPortfolio': work_portfolio
     }
-    print(user_profile)
     return render(request, 'grandpa/index.html', context)
